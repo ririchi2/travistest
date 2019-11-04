2c6644fc1f2b5d27e603427ed5ef07f7a6a2ba12
ririchi2 <richipineda2@gmail.com>
2019-11-04 20:50:06 +0000

    gif change 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-    res.send('<iframe src="http://giphygifs.s3.amazonaws.com/media/8beCqjMLtgKm4/giphy.gif" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
+    res.send('<iframe src="https://i.kym-cdn.com/photos/images/masonry/001/408/869/2b1.gif" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
 });
 
 app.listen(PORT, function () {
