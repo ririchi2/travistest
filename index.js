14abb1bcc3aeae10b0e14ff197ba5685248d3feb
ririchi2 <richipineda2@gmail.com>
2019-11-04 20:47:01 +0000

    gif change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-    res.send('<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
+    res.send('<iframe src="http://giphygifs.s3.amazonaws.com/media/8beCqjMLtgKm4/giphy.gif" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
 });
 
 app.listen(PORT, function () {
